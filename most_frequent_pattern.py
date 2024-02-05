550da5f8a12a758fe296c6095c68258975306ad5
Nisrine <nisrine@glanum.com>
2024-02-05 17:13:43 +0100

    most frequent pattern (apriori + fp growth)

--- most_frequent_pattern.py
+++ most_frequent_pattern.py
@@ -2,23 +2,26 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 from mlxtend.preprocessing import TransactionEncoder
-from mlxtend.frequent_patterns import apriori, association_rules
+from mlxtend.frequent_patterns import apriori, association_rules, fpgrowth
 
 
-def most_frequent_pattern_main_function(df_lines, products, transformed_sales_filter):
-    data = df_lines.merge(products, on='Product_ID').groupby(transformed_sales_filter + '_ID')['Product_name'].apply(lambda x: list(set(x))).to_frame()
+def clean_data(df_lines, products, transformed_sales_filter):
+    data = df_lines.merge(products, on='Product_ID').groupby(transformed_sales_filter + '_ID')['Product_name'].apply(
+        lambda x: list(set(x))).to_frame()
     data = list(data['Product_name'])
 
     te = TransactionEncoder()
     te_ary = te.fit(data).transform(data)
     df = pd.DataFrame(te_ary, columns=te.columns_)
+    return df
 
-    st.header('First approach - Apriori')
+
+def apriori_approach(df, min_support=0.001, metric="confidence", min_threshold=0.01):
     st.subheader('Most Frequent Items')
-    apriori_res = apriori(df, min_support=0.001, use_colnames=True).sort_values(by="support", ascending=False)
+    apriori_res = apriori(df, min_support=min_support, use_colnames=True).sort_values(by="support", ascending=False)
     bar_data = apriori_res[:5]
     bar_data['itemsets'] = bar_data['itemsets'].apply(list).astype(str)
-    st.write(px.bar(x=bar_data['itemsets'], y=bar_data['support'], labels={'x':'Product', 'y':'Support value'}))
+    st.write(px.bar(x=bar_data['itemsets'], y=bar_data['support'], labels={'x': 'Product', 'y': 'Support value'}))
 
     # apriori_res['length'] = apriori_res['itemsets'].apply(lambda x: len(x))
     # heatmap_data = apriori_res[apriori_res['length'] == 2].drop(['length'], axis=1)
@@ -28,24 +31,62 @@ def most_frequent_pattern_main_function(df_lines, products, transformed_sales_fi
     # st.write(px.imshow(heatmap_data))
 
     st.subheader('Association rules')
-    rules = association_rules(apriori_res, metric="lift", min_threshold=1).sort_values(by='lift', ascending=False)
+    rules = association_rules(apriori_res, metric=metric, min_threshold=min_threshold).sort_values(by='confidence',
+                                                                                                   ascending=False)
     rules['lhs items'] = rules['antecedents'].apply(lambda x: len(x))
     rules['antecedents_'] = rules['antecedents'].apply(lambda a: ','.join(list(a)))
     rules['consequents_'] = rules['consequents'].apply(lambda a: ','.join(list(a)))
-    st.write(rules)
 
-    pivot = rules[rules['lhs items'] > 1].pivot(
-        index='antecedents_', columns='consequents_', values='lift')
-
+    pivot = rules.pivot(index='antecedents_', columns='consequents_', values=metric)
     st.write(px.imshow(pivot[:5]))
+    return
+
+
+def fpgrowth_approach(df, min_support=0.001, metric='lift', min_threshold=0.01):
+    st.subheader('Most Frequent Items')
+    fpgrowth_res = fpgrowth(df, min_support=min_support, use_colnames=True).sort_values(by="support", ascending=False)
+    bar_data = fpgrowth_res[:5]
+    bar_data['itemsets'] = bar_data['itemsets'].apply(list).astype(str)
+    st.write(px.bar(x=bar_data['itemsets'], y=bar_data['support'], labels={'x': 'Product', 'y': 'Support value'}))
+
+    st.subheader('Association rules')
+    rules = association_rules(fpgrowth_res, metric=metric, min_threshold=min_threshold)
+    rules['lhs items'] = rules['antecedents'].apply(lambda x: len(x))
+    rules['antecedents_'] = rules['antecedents'].apply(lambda a: ','.join(list(a)))
+    rules['consequents_'] = rules['consequents'].apply(lambda a: ','.join(list(a)))
+
+    pivot = rules.pivot(index='antecedents_', columns='consequents_', values=metric)
+    st.write(px.imshow(pivot[:5]))
+    return
+
+
+def most_frequent_pattern_main_function(df_lines, products, transformed_sales_filter):
+    df = clean_data(df_lines, products, transformed_sales_filter)
+
+    col1, col2, col3 = st.columns(3)
+    min_support = col1.number_input('Insert min support', value=0.001, format='%f')
+    metric = col2.selectbox('Which metric do you choose ?',
+                          ('confidence', 'lift'))
+    min_threshold = col3.number_input('Insert min threshold', value=0.01, format='%f')
 
     with st.expander("See explanation"):
-        st.write("We can interpret our resulting table as follows.")
-        st.write("***Support Value ->*** indicates the rate of seeing antecedent and consequent together in all purchases.")
+        st.write(
+            "***Support Value ->*** indicates the rate of seeing antecedent and consequent together in all purchases.")
         st.write("***Confidence Value ->*** shows the percentage of customers who buy antecedent also buy consequent.")
-        st.write("***Lift Value ->*** shows by how much the sales of consequent increase for purchases with antecedent.")
-        st.write("***Leverage Value ->*** quantifies how much the occurrence of antecedent and consequent together deviates from what would be expected by chance.")
-        st.write("***Conviction Value ->*** quantifies how much the presence of antecedent implies the absence of consequent.")
-        st.write("***Zhang's metric ->*** measure designed to assess the strength of association (positive or negative) between two items, taking into account both their co-occurrence and their non-co-occurrence.")
+        st.write(
+            "***Lift Value ->*** shows by how much the sales of consequent increase for purchases with antecedent.")
+        st.write(
+            "***Leverage Value ->*** quantifies how much the occurrence of antecedent and consequent together deviates from what would be expected by chance.")
+        st.write(
+            "***Conviction Value ->*** quantifies how much the presence of antecedent implies the absence of consequent.")
+        st.write(
+            "***Zhang's metric ->*** measure designed to assess the strength of association (positive or negative) between two items, taking into account both their co-occurrence and their non-co-occurrence.")
+
+    st.header('First approach - Apriori')
+    apriori_approach(df, min_support, metric, min_threshold)
+
+    st.header('Second approach - FP growth')
+    fpgrowth_approach(df, min_support, metric, min_threshold)
+
 
     return
